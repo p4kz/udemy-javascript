14c0671a12d55b6624534084870b74e74a1b5319
p4kz <wolpak@hotmail.com>
2022-02-22 07:43:34 -0300

    aula8

---
+++ aula8/calculadora.js
@@ -0,0 +1,10 @@
+const nome = 'joão'
+const sobrenome = 'Nelson'
+const altura = 1.86;
+const peso = 80;
+const idade = 30
+let result = peso / (altura * altura);
+let anoDeNacimento = idade - 2022;
+
+console.log(result);
+console.log(anoDeNacimento)
---
+++ aula8/index.js
@@ -0,0 +1,15 @@
+// joão jão tem 30 anos, pesa 84 kilos  
+// e 1.80 de altura seu imc é de 25;99
+
+const nome =  'joão';
+const sobreNome = 'jão';
+const idade = 30;
+const peso = 84;
+const altura = 1.8;
+
+let imc = peso / (altura * altura);
+anoNacimento = 2019 - idade;
+
+console.log (`${nome} ${sobreNome}, tem ${idade} anos, pesa ${peso} kg`);
+console.log (`tem altura de ${altura} e seu imc é de ${imc}`);
+console.log (`${nome}, 'nasceu em' ${anoNacimento}.`);
