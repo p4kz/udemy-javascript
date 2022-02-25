07ec343704efd9a660a06696a5a7c29dec92384c
p4kz <wolpak@hotmail.com>
2022-02-25 13:04:56 -0300

    aula16 e aula17

---
+++ aula16/arrays.js
@@ -0,0 +1,40 @@
+//           0123
+// const nome ='Eric';
+// console.log(nome[3]);  //saber qial o index, localizaçãonumerica da letra 
+
+//                0        1        2           3        4   index
+const alunos = ['luiz', 'Maria', 'joão',]; // luiza, eduardo
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+// alunos.push ('luiza');
+// alunos.push ('Eduardo');
+
+// console.log(alunos.slice(0, 3)); //pegar uma orden de nomes 
+
+// delete alunos[1]; deletar um indice
+// console.log(alunos);
+
+// const removido = alunos.shift(alunos); // remove o primeiro elmento
+// const removido = alunos.pop(alunos); // remover o ultimo elemndo da arrays quardando ele em uma variavel 
+// console.log(alunos);
+// console.log(removido);
+
+// alunos.unshift('Marta'); // adiciona no commeço do arrays
+// alunos.push('Marta');   //adiciona automatico na ultima posição do arrays
+// alunos.push('Otavio');
+
+// console.log(alunos.length); // saber qual o numero do ultimo elemento do array
+
+// alunos[alunos.length] ='luiza';
+// alunos[alunos.length] ='Marta';
+// alunos[alunos.length] ='Josi';
+// alunos[alunos.length] ='Alex';
+
+// alunos[0] = 'Eduardo'; // editar um nome expico na arrays
+// alunos[3] = 'luiza';  // adicionar um nome ao arrays
+
+// console.log(alunos);
+// console.log(alunos[0]); //exibir a localização de um elemento expecifico
+// console.log(alunos[2]);
---
+++ aula17/function.js
@@ -0,0 +1,48 @@
+//function pode recebe valor, pode executar uma ação ou pode retornar valor 
+// a function não lê nada abixo do 'return' 
+
+// Exemplo
+
+        // function saudacao(nome){
+        //     console.log();
+        //     return `Bom dia ${nome}`;            
+        // } 
+        // const variavel = saudacao('marta');
+        // console.log(variavel);
+
+//-->________________________________________________
+
+        // function soma(x,y){
+        //     const resultado = x + y;
+        //     return resultado;
+        // }
+        // console.log(soma(2, 2));
+
+//-->________________________________________________
+
+        // function soma(x = 1,y = 2){
+        //     const resultado = x + y;
+        //     return resultado;
+        // }
+
+        // console.log(soma());
+
+
+
+//a arrow function substitui a palavra 
+//function no codigo e em alguns 
+//casos vale a pena usar a function escrita e 
+//outras não; o return tbm é abreviado   
+
+        // const raiz = function(n) {
+        //     return n ** 0.5;
+        // }
+        // console.log(raiz(9));
+        // console.log(raiz(16));
+        // console.log(raiz(25));
+//-->__________________________________________________
+
+        const raiz = n => n ** 0.5    
+        console.log(raiz(9));
+        console.log(raiz(16));
+        console.log(raiz(25));
