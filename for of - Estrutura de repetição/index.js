663f635d827530b04db093a0888d47e30fe9d92a
p4kz <wolpak@hotmail.com>
2022-03-09 14:05:37 -0300

    for and examples/ for of/ forEach

---
+++ for of - Estrutura de repetição/index.js
@@ -0,0 +1,33 @@
+
+
+// const nome = ['eric', 'otavio', 'maria', 'pak'];
+
+// for (let i = 0; i < nome.length; i++) {
+//     console.log(nome[i]);
+// }
+
+// for (let i in nome) {
+//     console.log(nome[i]);
+// }
+
+// for (let valor of nome) {  // for of não funciona em objeto 
+//  console.log(valor);
+// }
+
+// nome.forEach(function (valor, indice, array) {
+//     console.log(valor, indice, array)
+// });
+
+
+// for classico geralmente com interáveis (array ou strings)
+// for in retorna o indice ou chave (string, array ou objetos)
+// for of  retorna em si (iteráveis, arrays ou strings )
+
+const pessoa = {
+    nome: 'luiz',
+    sobrenome: 'otavio',
+}
+
+for (let i in pessoa) {
+    console.log(i, pessoa[i]);
+}
