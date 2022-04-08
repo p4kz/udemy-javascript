e4fb34342b33597dd0a0f5cc385861909c7a0d5e
p4kz <wolpak@hotmail.com>
2022-04-08 17:41:17 -0300

    create template webpack

---
+++ JavaScript - ES6 Modules/Webpack (boilerplate)/public/assets/js/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('fff');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
