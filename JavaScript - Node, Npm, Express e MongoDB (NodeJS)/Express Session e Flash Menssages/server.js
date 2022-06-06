35e519e44b13c34973b0416c527ff89414eca5a5
p4kz <wolpak@hotmail.com>
2022-06-06 06:13:46 -0300

    intro flash msg && express session

---
+++ JavaScript - Node, Npm, Express e MongoDB (NodeJS)/Express Session e Flash Menssages/server.js
@@ -0,0 +1,48 @@
+require('dotenv').config()
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.CONNECTION_STRING,{ useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.emit('pronto')
+  }).catch(e => console.log(e))
+
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session);
+const flash = require('connect-flash')
+
+const routes = require('./routes')
+const path = require('path')
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use(express.static(path.resolve(__dirname, 'public')))
+
+const sessionOptions = session({
+  secret: 'dasdsa 213 2113sda',
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true
+  },
+})
+
+app.use(sessionOptions)
+app.use(flash())
+
+app.set('views', path.resolve(__dirname, 'src', 'views'))
+app.set('view engine', 'ejs')
+
+app.use(routes);
+
+// so vai executar quando mongose emitir pronto
+app.on('pronto', () => { 
+  app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000')
+    console.log('Servidor executando na porta 3000')
+  })
+})
