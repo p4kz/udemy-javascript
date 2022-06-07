2d51ba1f844efb86db44c8150965c0fee738e7ce
p4kz <wolpak@hotmail.com>
2022-06-07 07:44:35 -0300

    fix sessionOptions

--- JavaScript - Node, Npm, Express e MongoDB (NodeJS)/Express Session e Flash Menssages/server.js
+++ JavaScript - Node, Npm, Express e MongoDB (NodeJS)/Express Session e Flash Menssages/server.js
@@ -10,7 +10,7 @@ mongoose.connect(process.env.CONNECTION_STRING,{ useNewUrlParser: true, useUnifi
   }).catch(e => console.log(e))
 
 const session = require('express-session')
-const MongoStore = require('connect-mongo')(session);
+const MongoStore = require('connect-mongo')
 const flash = require('connect-flash')
 
 const routes = require('./routes')
@@ -22,7 +22,7 @@ app.use(express.static(path.resolve(__dirname, 'public')))
 
 const sessionOptions = session({
   secret: 'dasdsa 213 2113sda',
-  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  store: MongoStore.create({ mongoUrl: process.env.CONNECTION_STRING }),
   resave: false,
   saveUninitialized: false,
   cookie: {
